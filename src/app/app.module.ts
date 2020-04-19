cfb43210d6b3e5e82f21c7f839d61928da138054
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-04-19 15:13:37 +0530

    Added: Spinner if data is not loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CityComponent } from './city/city.component';
 import {HttpClientModule} from '@angular/common/http';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [AppComponent, CityComponent],
-  imports: [BrowserModule, FormsModule,HttpClientModule],
+  imports: [BrowserModule, FormsModule,HttpClientModule,MatProgressSpinnerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
