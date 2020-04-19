7ee82f8811fc79a35c6b265ae9f22db0ba6e8761
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-04-19 15:18:47 +0530

    Added: pie chart

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IPatient } from './patient';
 import { PatientService } from './patient.service';
+import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'cs-city',
@@ -39,5 +40,33 @@ export class CityComponent implements OnInit {
       },
       error: (err) => (this.errorMessage = err),
     });
+
+    let chart = new CanvasJS.Chart('chartContainer', {
+      theme: 'light2',
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: 'Monthly Expense',
+      },
+      data: [
+        {
+          type: 'pie',
+          showInLegend: true,
+          toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
+          indexLabel: '{name} - #percent%',
+          dataPoints: [
+            { y: 450, name: 'Food' },
+            { y: 120, name: 'Insurance' },
+            { y: 300, name: 'Traveling' },
+            { y: 800, name: 'Housing' },
+            { y: 150, name: 'Education' },
+            { y: 150, name: 'Shopping' },
+            { y: 250, name: 'Others' },
+          ],
+        },
+      ],
+    });
+
+    chart.render();
   }
 }
