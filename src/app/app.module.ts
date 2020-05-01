3091b69354613ce7e0d1e85f562189e89d097f3c
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-05-01 12:43:51 +0530

    Added scrollbar for table and converted patient-stats-component to shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { PatientComponent } from './pages/patient/patient.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { PatientStatsComponent } from './pages/patient-stats/patient-stats.component';
-import { HomeComponent } from './pages/home/home.component';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {PatientComponent} from './pages/patient/patient.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {PatientStatsComponent} from './pages/patient-stats/patient-stats.component';
+import {HomeComponent} from './pages/home/home.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, PatientComponent, PatientStatsComponent],
+  declarations: [AppComponent, PatientComponent, PatientStatsComponent, HomeComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     MatProgressSpinnerModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent},
-      { path: 'home', component: HomeComponent},
-      { path: 'patients', component: PatientComponent },
+      {path: '', component: HomeComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'patients', component: PatientComponent},
     ]),
   ],
   providers: [],
